5c1963b9b4083a03bdc9f9df2c6b1a43ab2337ae
Stacey McKnight <mcknight.stm@gmail.com>
2021-02-22 15:55:19 -0600

    commit tree 29e4b95f43d9f3923f02a8ec030ef2bacccd0de7

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class StudentsController < ApplicationController
   def new
     @student = Student.new
@@ -21,7 +22,7 @@ class StudentsController < ApplicationController
   end
 
   def index
-    @students = Student.all
+    @students = Student.search(params[:query])
   end
 
   def student_params
